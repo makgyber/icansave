6d4ee82b69f79539f0f7b3ed840fa3d9cc5cc1ad
Julius Mwesigwa <36412999+Juliusmwesigwa@users.noreply.github.com>
2021-04-23 08:44:45 +0300

    Updates db

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 module.exports = {
-  // HOST: "157.230.82.8",
-  // USER: "amvunidbadmin",
-  // PASSWORD: "Amvuni@2021",
+  HOST: "157.230.82.8",
+  USER: "amvunidbadmin",
+  PASSWORD: "Amvuni@2021",
   
 
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
+    // HOST: "localhost",
+    // USER: "root",
+    // PASSWORD: "",
     DB: "icansave",
     PORT:3306,
     dialect: "mysql",
