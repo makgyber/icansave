48cd6e37f7d037786cb230c280fc2ff0020267f5
Julius Mwesigwa <36412999+Juliusmwesigwa@users.noreply.github.com>
2021-05-24 04:11:44 +0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,33 +1,3 @@
-// module.exports = {
-//   HOST: "46.101.248.233",
-//   USER: "julius",
-//   PASSWORD: "JesusisLord@20",
-//   DB: "naangirisa",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
-////Development
-// module.exports = {
-//   HOST: "localhost",
-//   USER: "naangadmin",
-//   PASSWORD: "Naangirisa@2021#",
-//   DB: "naangirisa",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   }
-// };
-
-
 
 module.exports = {
   // HOST: "xxxxxxxx",
@@ -36,7 +6,7 @@ module.exports = {
   HOST: "localhost",
   USER: "root",
   PASSWORD: "",
-  DB: "icansave2",
+  DB: "icansave",
   dialect: "mysql",
   pool: {
     max: 5,
